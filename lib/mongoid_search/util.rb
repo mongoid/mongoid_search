09325eb78ef74c0d4ea78588ce52526dc5ecf7cf
Emil Kampp <emil@kampp.me>
2012-06-02 09:57:36 +0200

    Better support for array of keys in hash

--- lib/mongoid_search/util.rb
+++ lib/mongoid_search/util.rb
@@ -12,12 +12,14 @@ module Util
             else
               attribute.map(&method).map { |t| Util.normalize_keywords t, stem_keywords, ignore_list }
             end
-          else
-            if attribute.respond_to?(method)
-              Util.normalize_keywords(attribute.send(method), stem_keywords, ignore_list)
-            else # If the field is a serialized hash
-              Util.normalize_keywords(klass.send(key)[method.to_sym], stem_keywords, ignore_list)
+          elsif attribute.is_a?(Hash)
+            if method.is_a?(Array)
+              method.map {|m| Util.normalize_keywords attribute[m.to_sym], stem_keywords, ignore_list }
+            else
+              Util.normalize_keywords(attribute[method.to_sym], stem_keywords, ignore_list)
             end
+          else
+            Util.normalize_keywords(attribute.send(method), stem_keywords, ignore_list)
           end
         end
       end
