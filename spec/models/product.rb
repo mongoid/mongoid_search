fcd0a5c66f04a34aed598bab293fb731bd1cbc12
Emil Kampp <emil@kampp.me>
2012-06-05 20:55:14 +0200

    Adding tests

--- spec/models/product.rb
+++ spec/models/product.rb
@@ -4,10 +4,12 @@ class Product
   field :brand
   field :name
   field :attrs, :type => Array
+  field :info, :type => Hash
 
   references_many :tags
   referenced_in   :category
   embeds_many     :subproducts
 
-  search_in :brand, :name, :outlet, :attrs, :tags => :name, :category => :name, :subproducts => [:brand, :name]
+  search_in :brand, :name, :outlet, :attrs, :tags => :name, :category => :name,
+            :subproducts => [:brand, :name], :info => [ :summary, :description ]
 end
